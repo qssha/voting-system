2736804868877631dbd1420d39dfc9e23485ec34
qssha <qssha@yandex.ru>
2020-08-29 03:50:46 +0300

    Logging added

--- src/main/java/com/voting/web/RootController.java
+++ src/main/java/com/voting/web/RootController.java
@@ -1,14 +1,19 @@
 package com.voting.web;
 
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 @Controller
 public class RootController {
+    private final static Logger log = getLogger(RootController.class);
 
     @GetMapping("/")
     public @ResponseBody String root() {
+        log.debug("FIRST DEBUG");
         return "OK\n";
     }
 }
