8961011f0e8c2e1d42c934cdd3da7b11f4e79382
qssha <qssha@yandex.ru>
2020-09-10 14:13:20 +0300

    AdminDishControllerTest impl

--- src/test/java/com/voting/web/dish/AdminDishControllerTest.java
+++ src/test/java/com/voting/web/dish/AdminDishControllerTest.java
@@ -1,7 +1,77 @@
 package com.voting.web.dish;
 
+import com.voting.model.Dish;
+import com.voting.service.DishService;
+import com.voting.util.exception.NotFoundException;
 import com.voting.web.AbstractControllerTest;
+import com.voting.web.json.JsonUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static com.voting.DishTestData.*;
+import static com.voting.TestUtil.readFromJson;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class AdminDishControllerTest extends AbstractControllerTest {
+    private final static String REST_URL = AdminDishController.REST_URL + "/";
 
+    @Autowired
+    private DishService dishService;
+
+    @Test
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + FIRST_DISH_ID))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(DISH_MATCHER.contentJson(FIRST_DISH));
+    }
+
+    @Test
+    void getAll() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(DISH_MATCHER.contentJson(getAllDishesSortedByName()));
+    }
+
+    @Test
+    void delete() throws Exception {
+        perform(MockMvcRequestBuilders.delete(REST_URL + FIRST_DISH_ID))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+        assertThrows(NotFoundException.class, () -> dishService.get(FIRST_DISH_ID));
+    }
+
+    @Test
+    void update() throws Exception {
+        Dish updated = getUpdated();
+        perform(MockMvcRequestBuilders.put(REST_URL + FIRST_DISH_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(updated)))
+                .andExpect(status().isNoContent());
+        DISH_MATCHER.assertMatch(dishService.get(FIRST_DISH_ID), updated);
+    }
+
+    @Test
+    void createWithLocation() throws Exception {
+        Dish newDish = getNew();
+        ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(newDish)))
+                .andExpect(status().isCreated());
+
+        Dish created = readFromJson(action, Dish.class);
+        int newId = created.getId();
+        newDish.setId(newId);
+        DISH_MATCHER.assertMatch(created, newDish);
+        DISH_MATCHER.assertMatch(dishService.get(newId), newDish);
+    }
 }
